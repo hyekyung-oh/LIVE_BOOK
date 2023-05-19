0f0f9bd16b11fa43bf645038687ed673eec37a94
Donghun <ehdgnstla@naver.com>
2023-05-19 15:24:51 +0900

    imageCreate.py 썸네일생성, main.py 썸네일 db 입력

--- python/imageCreate.py
+++ python/imageCreate.py
@@ -6,6 +6,50 @@ import openai
 import sys
 import tkinter as tk
 from tkinter import filedialog
+from tqdm import tqdm
+
+# 내용 요약 함수
+def summarize_text(text):
+    model_engine = "text-davinci-002"
+    prompt = (f"Summarize the content in three sentences, \n\n{text}")
+    completions = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=60,
+        n=1,
+        stop=None,
+        temperature=1,
+    )
+    message = completions.choices[0].text.strip()
+    return message
+
+# 입력받은 책제목, 페이지, 요약된 내용을 입력 받아 이미지 생성
+def createImage(summarize_text, book_title, pno) :
+    try : 
+        res_img = openai.Image.create(
+            prompt=f'Book illustration, {summarize_text}',
+            n=1,
+            size="256x256"
+        )
+        
+        img_url = res_img['data'][0]['url']
+        img_path = f'../temp/{book_title}/{book_title}_{pno}.png'
+        
+        urllib.request.urlretrieve(img_url, img_path)
+    
+    except openai.error.InvalidRequestError as ex :
+        
+        print("예외가 발생했습니다." , ex)
+        createImage(summarize_text, book_title, pno)
+
+#썸네일 생성 함수
+def thumnail_fromPDF(doc, book_title) :
+    page = doc[0]
+    img = page.get_pixmap()
+    img.save(f"../temp/{book_title}/{book_title}_thumnail.png")
+        
+        
+# ---------- main ---------- # 
 
 # 파일 선택 윈도우 띄우기 # 
 root = tk.Tk()
@@ -25,46 +69,30 @@ book_title = os.path.splitext(filename)[0] # 파일명에서 확장자 제거
 os.makedirs(f'../temp/{book_title}', exist_ok=True)
 
 # api 키는 push할때 초기화 됨. 동훈한테 문의해서 테스트시 api키를 받으세여
-openai.api_key = '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'
+openai.api_key = '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'
 
-count = 0
-start_pno = 6
-
-# 내용 요약 함수
-def summarize_text(text):
-    model_engine = "text-davinci-002"
-    prompt = (f"Summarize the content in three sentences, \n\n{text}")
-    completions = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=60,
-        n=1,
-        stop=None,
-        temperature=1,
-    )
-    message = completions.choices[0].text.strip()
-    return message
+# 썸네일 생성
+thumnail_fromPDF(doc, book_title)
 
 
-# 각 페이지를 요약내용 바탕 이미지 생성
-for pno in range(start_pno, doc.page_count) :
+startPno = int(input("이미지를 생성할 페이지의 시작점을 정해주세요 \n>>"))
+lastPg = int(input("이미지를 생성할 마지막 페이지를 입력해주세요 \n>>"))
+
+for pno in tqdm(range(startPno, lastPg+1)) :
     text = doc.get_page_text(pno=pno)
 
+    
     # Preprocess text
     text = re.sub(r"\s+", " ", text)
     # Remove page number
     text = ' '.join(text.split(' ')[:-1])
     
-    res_img = openai.Image.create(
-        prompt=f'Book illustration, {summarize_text(doc[pno])}',
-        n=1,
-        size="256x256"
-    )
+    contents = summarize_text(text)
+    print("데이터 확인!!--------")
+    print(contents)
+    print("데이터 확인!!--------")
     
-    img_url = res_img['data'][0]['url']
-    img_path = f'../temp/{book_title}/{pno}.jpg'
-    
-    print(str(pno) + "페이지입니다!!!---" + summarize_text(doc[pno]))
-    urllib.request.urlretrieve(img_url, img_path)
-
+    print(pno, "페이지 이미지 생성 중입니다.\n")
+    createImage(contents, book_title, pno)
+    print("성공")
 
